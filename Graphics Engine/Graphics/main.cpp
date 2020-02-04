ae91cd807ae17586ba7e2e56c6cef03782eba904
Michael <mjtarmstrong@gmail.com>
2020-02-04 13:37:06 +1100

    Added Shaders and Error messages
    Is currently working

--- Graphics Engine/Graphics/main.cpp
+++ Graphics Engine/Graphics/main.cpp
@@ -2,6 +2,8 @@
 #include "gl_core_4_5.h"
 #include "glfw3.h"
 #include "ext.hpp"
+#include <fstream>
+#include <sstream>
 
 using uint = unsigned int;
 
@@ -33,7 +35,7 @@ int main()
 	auto minor = ogl_GetMinorVersion();
 	printf("GL: %i.%i\n", major, minor);
 
-	//
+	// Create mesh data
 	glm::vec3 verticies[] =
 	{
 		glm::vec3(-0.5f, 0.5f, 0.0f),
@@ -44,20 +46,10 @@ int main()
 		glm::vec3( 0.5f, -0.5f, 0.0f)
 	};
 
-	glm::mat4 projection = glm::perspective(90.0f, 16 / 9.0f, 0.1f, 5.0f);
-	glm::mat4 view = glm::lookAt(glm::vec3(0, 0, 1), glm::vec3(1,0, 0), glm::vec3(0, 1, 0));
-	glm::mat4 model = glm::mat4(1);
-
-	glm::mat4 pvm = projection * view * model;
-
-	for (int i = 0; i < 6; ++i)
-	{
-		verticies[i] = (glm::vec3)(pvm * glm::vec4(verticies[i], 1));
-	}
-
+	// Create and Load mesh
 	uint VAO;
 	uint VBO;
-	// int IBO;
+	//// int IBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
@@ -72,11 +64,109 @@ int main()
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	/** CAMERA **/
+	glm::mat4 projection = glm::perspective(90.0f, 16 / 9.0f, 0.1f, 5.0f);
+	glm::mat4 view = glm::lookAt(glm::vec3(0, 0, 1), glm::vec3(0 ,0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 model = glm::mat4(1);
 
-	// Keeps the window open till the Escape key is pressed
+	uint vertex_shader_ID = 0;
+	uint fragment_shader_ID = 0;
+	uint shader_program_ID = 0;
+
+	// Load shader from file into string
+	std::string shader_data;
+	std::ifstream in_file_stream("..\\Shaders\\simple_vertex.glsl", std::ifstream::in);
+
+	// Load the source into a string for compilation
+	std::stringstream string_stream;
+	if (in_file_stream.is_open())
+	{
+		string_stream << in_file_stream.rdbuf();
+		shader_data = string_stream.str();
+		in_file_stream.close();
+	}
+
+	// Allocate space for shader program
+	vertex_shader_ID = glCreateShader(GL_VERTEX_SHADER);
+	// Convert to raw char*
+	const char* data = shader_data.c_str();
+	// Send in the char* tp shader location
+	glShaderSource(vertex_shader_ID, 1, (const GLchar**)&data, 0);
+	// Build
+	glCompileShader(vertex_shader_ID);
+
+	// Is it working
+	// Check the shader compiled
+	GLint success = GL_FALSE;
+	glGetShaderiv(vertex_shader_ID, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		printf("Vertex shader FAILED\n");
+	}
+
+	/** PART2 **/
+	std::ifstream in_file_stream_frag("..\\Shaders\\simple_frag.glsl", std::ifstream::in);
+
+	
+	// Load the source into a string for compilation
+	std::stringstream frag_string_stream;
+	if (in_file_stream_frag.is_open())
+	{
+		frag_string_stream << in_file_stream_frag.rdbuf();
+		shader_data = frag_string_stream.str();
+		in_file_stream_frag.close();
+	}
+
+	// Allocate space for shader program
+	fragment_shader_ID = glCreateShader(GL_FRAGMENT_SHADER);
+	// Convert to raw char*
+	data = shader_data.c_str();
+	// Send in the char* tp shader location
+	glShaderSource(fragment_shader_ID, 1, (const GLchar**)&data, 0);
+	// Build
+	glCompileShader(fragment_shader_ID);
+
+	// Is it working
+	// Check the shader compiled
+	success = GL_FALSE;
+	glGetShaderiv(fragment_shader_ID, GL_COMPILE_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		printf("Fragment shader FAILED\n");
+	}
+
+	shader_program_ID = glCreateProgram();
+
+	glAttachShader(shader_program_ID, vertex_shader_ID);
+	glAttachShader(shader_program_ID, fragment_shader_ID);
+
+	glLinkProgram(shader_program_ID);
+
+	success = GL_FALSE;
+	glGetProgramiv(shader_program_ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		printf("Shader linking FAILED");
+	}
+
+	// Game Loop
 	while (glfwWindowShouldClose(window) == false && 
 			glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
 	{
+		//glm::mat4 pvm = projection * view * model;
+
+		glm::mat4 pv = projection * view;
+
+		glm::vec4 color = glm::vec4(0.5f);
+
+		glUseProgram(shader_program_ID);
+		auto uniform_location = glGetUniformLocation(shader_program_ID, "projection_view_matrix");
+		glUniformMatrix4fv(uniform_location, 1, false, glm::value_ptr(pv));
+		uniform_location = glGetUniformLocation(shader_program_ID, "model_matrix");
+		glUniformMatrix4fv(uniform_location, 1, false, glm::value_ptr(model));
+		uniform_location = glGetUniformLocation(shader_program_ID, "color");
+		glUniform4fv(uniform_location, 1, glm::value_ptr(color));
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
